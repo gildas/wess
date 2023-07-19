85c0037a468e6f33bb02e04ed8ceb8cd129d0782
Gildas Cherruel <gildas@breizh.org>
2023-07-19 18:46:34 +0900

    More samples

---
+++ samples/probes/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+
+	"github.com/gildas/go-core"
+	"github.com/gildas/go-logger"
+	"github.com/gildas/wess"
+)
+
+var APP = "sample-probes"
+
+func main() {
+	port := flag.Int("port", core.GetEnvAsInt("PORT", 80), "The port to listen on")
+	probePort := flag.Int("probeport", core.GetEnvAsInt("PROBES", 7000), "The probe port to listen on")
+	flag.Parse()
+
+	log := logger.Create(APP)
+	defer log.Flush()
+	log.Infof(strings.Repeat("-", 80))
+	log.Infof("Starting %s v%s (%s)", APP, wess.VERSION, runtime.GOARCH)
+	log.Infof("Log Destination: %s", log)
+
+	if *port == 0 {
+		log.Fatalf("No port specified")
+		os.Exit(-1)
+	}
+
+	server := wess.NewServer(wess.ServerOptions{
+		Port:      *port,
+		ProbePort: *probePort,
+		Logger:    log,
+	})
+
+	server.AddRouteWithFunc("GET", "/hello", func(w http.ResponseWriter, r *http.Request) {
+		log := logger.Must(logger.FromContext(r.Context())).Child(nil, "hello")
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "text/plain")
+		written, _ := w.Write([]byte("Hello, World!"))
+		log.Debugf("Witten %d bytes", written)
+	})
+
+	shutdown, _ := server.Start(context.Background())
+	<-shutdown
+}
